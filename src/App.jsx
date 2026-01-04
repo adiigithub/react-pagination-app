1a77737857fd549ea4bfebc046214902e6366211
Aditya Tripathi <aditya.tripathi.in@gmail.com>
2026-01-05 05:02:43 +0530

    Improve responsive UI and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import FetchUser from "./components/FetchUser"
+
 import Pagination from "./components/Pagination"
-import Search from "./components/SearchBar"
+
 
 const App = ()=>{
   return (
     <>
-    {/* <Search/>
-    <FetchUser/> */}
+   
     <Pagination/>
     </>
   )
