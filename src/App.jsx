7b01d952925edf90f8f41d010c10e585a26b9dd6
Aditya Tripathi <126022321+adiigithub@users.noreply.github.com>
2026-01-05 04:47:01 +0530

    Remove unused components from App
    Removed unused components Search and FetchUser from App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import FetchUser from "./components/FetchUser"
+
 import Pagination from "./components/Pagination"
-import Search from "./components/SearchBar"
+
 
 const App = ()=>{
   return (
     <>
-    {/* <Search/>
-    <FetchUser/> */}
     <Pagination/>
     </>
   )
